ed39b71c393d5f8c7ec2ca91f4639e6c1f09b7d8
Adam LaTorraca <adam.latorraca@gmail.com>
2020-05-18 20:42:10 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,12 +15,10 @@ class Song
   end
 
   def self.new_by_filename(filename)
-    file = filename.chomp(".mp3").split(" - ")
-    file.each do |name|
-      self.name = file[1]
-      self.artist = file[0]
-      end
-    @@all << self.name.artist
+  artist_name, song_name, extra = filename.split(" - ")
+  song = self.new(song_name)
+  artist = Artist.find_or_create_by_name(artist_name)
+  artist.add_song(song)
   end
 
   def artist=(name)
