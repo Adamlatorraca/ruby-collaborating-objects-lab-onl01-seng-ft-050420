94d67ab947a989f0e8c7c86e776910b406b3d991
Adam LaTorraca <adam.latorraca@gmail.com>
2020-05-18 18:48:29 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,7 +14,7 @@ class Song
 
   def self.new_by_filename(filename)
     song = self.new
-    song.name = filename.split(" - ")[1]
+    song = filename.split(" - ")[1]
   end
 
   def artist_name=(name)
