1aab100ef55a30d0692b71c94eea06518d2434b5
Adam LaTorraca <adam.latorraca@gmail.com>
2020-05-18 17:41:39 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -10,8 +10,8 @@ class MP3Importer
     Dir.new(self.path).each do |file|
         list_of_filenames << file if file.length > 4
       end
-      list_of_filenames
-    end
+    list_of_filenames
+  end
 
   def import(list_of_filenames)
     self.list_of_filenames.each{ |filename| Song.new_by_filename(filename) }
