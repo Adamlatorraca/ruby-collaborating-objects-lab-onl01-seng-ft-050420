e7d801d5247856787b44f239a049bcf16f091c0d
Adam LaTorraca <adam.latorraca@gmail.com>
2020-05-18 18:31:31 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -21,7 +21,7 @@ class Artist
   end
 
   def self.find_or_create_by_name(name)
-    if self.artist.nil?
+    if (self.artist.nil?)
       self.artist = Artist.new(name)
     else
       self.artist.name = name
